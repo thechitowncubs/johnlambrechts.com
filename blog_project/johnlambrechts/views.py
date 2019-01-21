72631d3cc88e1a14e7e31379d20489f98e1897c2
John Lambrechts <johnlambrechts@gmail.com>
2019-01-21 11:27:45 -0600

    fix views field

--- blog_project/johnlambrechts/views.py
+++ blog_project/johnlambrechts/views.py
@@ -45,7 +45,7 @@ class DraftListView(LoginRequiredMixin,ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__isnull=True).order_by('create_date')
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
 
 #################
 #COMMENTS
